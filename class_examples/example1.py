1cf4d5520e209982ee518f78824d572178869aaa
jschaefMB <jschaefmn@gmail.com>
2024-02-08 18:33:20 -0600

    finished example1 and 2

--- class_examples/example1.py
+++ class_examples/example1.py
@@ -2,21 +2,26 @@ class Greeter:
   # def __init__(self, name):
     # self.name = name
   
-  def __init__(self, input_name, puncuation):
-    self.name = input_name
-    self.punctuation = puncuation
+  def __init__(self, input_name, puncuation): # Constructor
+    self.name = input_name # Attribute
+    self.punctuation = puncuation # Attribute
     
-  def morning_greeting(self):
+  def morning_greeting(self): # Instance Method
     print(f'Good morning, {self.name}{self.punctuation}')
     
-  def evening_greeting(self):
+  def evening_greeting(self): # Instance Method
     print(f'Good morning, {self.name}{self.punctuation}')
     
-example_greeter1 = Greeter('Erik', '.')
-example_greeter2 = Greeter('Joey', '!')
+  def general_greeting(): # Static Method
+    print("Hello, there")
+    
+example_greeter1 = Greeter('Erik', '.') #instance of greeter | greeter object
+example_greeter2 = Greeter('Joey', '!') #instance of greeter | greeter object
 
 example_greeter1.morning_greeting()
 example_greeter2.morning_greeting()
+
+Greeter.general_greeting()
 
 # print(
 #   example_greeter1.name
---
+++ class_examples/example3.py
@@ -0,0 +1,9 @@
+class StaticGreeter:
+  def morning_greeting():
+    print('Good morning')
+    
+  def evening_greeting():
+    print('Good evening')
+    
+StaticGreeter.morning_greeting()
+StaticGreeter.evening_greeting()
